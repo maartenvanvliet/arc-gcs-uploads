971d5658324194e76380766da0bda8b3490ee19a
Maarten van Vliet <maartenvanvliet@gmail.com>
2017-08-05 18:07:43 +0200

    Add Arc/Arc.Ecto/Arc.Gcs dependencies

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule App.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:arc, "~> 0.8.0"},
+      {:arc_ecto, "~> 0.7.0"},
+      {:arc_gcs, "~> 0.0.2"}
     ]
   end
 
